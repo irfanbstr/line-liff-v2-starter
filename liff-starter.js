2a55a483f34645187c24d38628bfcac2ae174d24
irfanbstr <irfanbstr@gmail.com>
2021-01-10 11:37:47 +0700

    liff id change

--- liff-starter.js
+++ liff-starter.js
@@ -1,6 +1,6 @@
 window.onload = function() {
     const useNodeJS = false;   // if you are not using a node server, set this value to false
-    const defaultLiffId = "1655333814-dvlkZqOv";   // change the default LIFF value if you are not using a node server
+    const defaultLiffId = "1655539275-LnQZ82GO";   // change the default LIFF value if you are not using a node server
 
     // DO NOT CHANGE THIS
     let myLiffId = "";
